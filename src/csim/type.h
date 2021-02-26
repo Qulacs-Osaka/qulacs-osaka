9b5d4cb179106afcb46412f2fdae6aebe374e236
ikanago <ikanago@ikanago.dev>
2021-02-26 16:06:20 +0900

    Fix type.h for compile error

--- src/csim/type.h
+++ src/csim/type.h
@@ -35,6 +35,7 @@ inline static double cimag(CTYPE val) { return std::imag(val); }
 #else
 typedef double _Complex CTYPE;
 #if __GNUC__ >= 8
+#ifndef __APPLE__
 #ifdef __cplusplus
 inline static double creal(CTYPE val) { return __real__ val; }
 inline static double cimag(CTYPE val) { return __imag__ val; }
@@ -43,6 +44,7 @@ inline static double cabs(CTYPE val) {
     double im = __imag__ val;
     return re * re + im * im;
 }
+#endif
 #endif
 #endif
 #endif
