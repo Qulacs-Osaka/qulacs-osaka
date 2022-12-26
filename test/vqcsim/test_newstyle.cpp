168684ec5d5dbaabddd62abcc76829ba6d01bf13
KowerKoint <50831431+KowerKoint@users.noreply.github.com>
2022-12-26 18:08:29 +0900

    Update test/vqcsim/test_newstyle.cpp
    Co-authored-by: Hiroya-W <hyuyukun@gmail.com>

--- test/vqcsim/test_newstyle.cpp
+++ test/vqcsim/test_newstyle.cpp
@@ -63,7 +63,6 @@ TEST(ParametricCircuit, GateApplyNew) {
     ParametricQuantumCircuit* circuit = new ParametricQuantumCircuit(n);
     Random random;
     for (UINT d = 0; d < depth; ++d) {
-        std::string d_str = std::to_string(d);
         for (UINT i = 0; i < n; ++i) {
             circuit->add_parametric_RX_gate_new_parameter(i, random.uniform());
             circuit->add_parametric_RY_gate_new_parameter(i, random.uniform());
